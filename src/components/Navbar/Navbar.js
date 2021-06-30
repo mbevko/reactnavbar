85e5c94cc5341fe0db156bfd174a1a80172d4a4d
Michael Evko <mbevko@gmail.com>
2021-06-30 11:49:22 -0400

    tutorial timestamp 3327

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import MenuItems from './MenuItems';
+import './Navbar.css';
 
 class Navbar extends React.Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked})
+    }
+
     render() {
         return (
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React</h1>
-                <div className="menu-icon">
-                    
+                <h1 className="navbar-logo">React <i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
+                <ul>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}><a className={item.cName} href={item.url}>
+                                {item.title}    
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
             </nav>
         )
     }
 }
+
+export default Navbar
+
+//must do an arrow function
