073feb735f3a77089bcad6b358c43168c71447b7
Michael Evko <mbevko@gmail.com>
2021-06-30 11:12:04 -0400

    editing

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">React</h1>
+                <div className="menu-icon">
+                    
+                </div>
+            </nav>
+        )
+    }
+}
